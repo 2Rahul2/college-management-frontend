a064412f062f59e84b533a7c9a5042f0a4cd9e0c
2Rahul2 <rahulbclg24@gmail.com>
2024-11-21 16:33:49 +0530

    e

--- src/components/TabContents/FacultyTabs/AddStudents.jsx
+++ src/components/TabContents/FacultyTabs/AddStudents.jsx
@@ -31,7 +31,7 @@ const AddStudents = ({notification}) => {
               method: 'POST',
               headers: {
                   'Content-Type': 'application/json',  // Assuming you are sending JSON data
-                  'X-CSRFToken': csrfToken,  // Include CSRF token in the header
+                  'x-csrftoken:': csrfToken,  // Include CSRF token in the header
               },
               body: JSON.stringify(values),
               credentials: 'include',  // Important: This ensures cookies (like CSRF tokens) are included in the request
